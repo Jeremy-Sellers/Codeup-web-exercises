39c0b32bcf53d6e667da4fe80a00f2a35feb11cf
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-03-02 15:13:52 -0600

    completed mini exercise for DOM

---
+++ square-mini-exercise.js
@@ -0,0 +1,8 @@
+// alert('hi');
+
+var squareButton = document.querySelector('.square');
+
+function squareClicked(){
+    alert('I am a big red square');
+}
+squareButton.addEventListener('click',squareClicked);
